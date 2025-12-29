dc2c401e7221d63d97b340a90322b3424a7d4c69
NotVersed <166773515+NotVersed@users.noreply.github.com>
2025-12-28 21:34:16 -0600

    made image in piecepanel final

--- src/PiecePanel.java
+++ src/PiecePanel.java
@@ -3,29 +3,29 @@ import java.awt.Image;
 import javax.swing.JPanel;
 
 public class PiecePanel extends JPanel {
-    private Image image;
+    private final Image IMAGE;
 
     public PiecePanel(Piece piece) {
-        this.image = piece.getPieceImage().getImage();
+        this.IMAGE = piece.getPieceImage().getImage();
         setOpaque(false);
     }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (image != null) {
+        if (IMAGE != null) {
             int width = getWidth();
             int height = getHeight();
             int padding = Math.min(width, height) / 4;
 
-            double scale = Math.min((double)(width - padding) / image.getWidth(null),
-                                    (double)(height - padding) / image.getHeight(null));
-            int newWidth = (int)(image.getWidth(null) * scale);
-            int newHeight = (int)(image.getHeight(null) * scale);
+            double scale = Math.min((double)(width - padding) / this.IMAGE.getWidth(null),
+                                    (double)(height - padding) / this.IMAGE.getHeight(null));
+            int newWidth = (int)(this.IMAGE.getWidth(null) * scale);
+            int newHeight = (int)(this.IMAGE.getHeight(null) * scale);
 
             int x = (width - newWidth) / 2;
             int y = (height - newHeight) / 2;
 
-            g.drawImage(image, x, y, newWidth, newHeight, null);
+            g.drawImage(this.IMAGE, x, y, newWidth, newHeight, null);
         }
     }
 
